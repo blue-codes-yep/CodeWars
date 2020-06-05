faaec30ff9506b85ff5dbc8107a36a4c0c3d3928
Blue Lindsey <linzblue5@gmail.com>
2020-06-04 19:17:33 -0500

    Simple Hello world function to start out my CodeWars adventure!

---
+++ helloWorld8kyu.js
@@ -0,0 +1,9 @@
+///// Make a simple function called greet that returns the most-famous "hello world!".
+/// Title : Function 1 -hello world
+
+const greet = () => {
+    const hello = "hello world!"
+
+    return hello;
+
+}
