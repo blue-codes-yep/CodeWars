995d8edab63a5e37149895a7dd5f63a53e1dfa4e
Blue Lindsey <linzblue5@gmail.com>
2020-06-04 19:28:30 -0500

    Solution for Beginner - Lost Without a Map kata

---
+++ LostWithoutAMap8Kyu.js
@@ -0,0 +1,8 @@
+/// Given an array of integers, return a new array with each value doubled.
+/// Title: Beginner - Lost Without a Map
+
+function maps(x) {
+    const double = x.map(i => i * 2)
+
+    return double;
+}
